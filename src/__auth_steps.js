1df1ad40a5aad190beb5106a56eb47c416a502ae
rezainiet <masudrezaog@gmail.com>
2022-04-10 14:30:44 +0600

    initial firebase setup

---
+++ src/__auth_steps.js
@@ -0,0 +1,10 @@
+/**
+ * 1. Create a new project in console.google.com
+ * 2. npm install firebase
+ * create firebase.init.js and import getAuth to export app.
+ * 
+*/
+
+/**
+ * Hosting Steps!
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB1psJ8S1KOYZZzfSjEp-VrXfPvA0Fva4g",
+    authDomain: "ema-john-simple-ecad5.firebaseapp.com",
+    projectId: "ema-john-simple-ecad5",
+    storageBucket: "ema-john-simple-ecad5.appspot.com",
+    messagingSenderId: "305980633442",
+    appId: "1:305980633442:web:8295c9bbeb6ec0d83abc3b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
